8bb34030b9895e50ea6246534fe0bc606112d7fd
thebakokid-does-code <80925485+thebakokid-does-code@users.noreply.github.com>
2021-07-29 10:54:26 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <html>
   <head>
     <title>PHP Test</title>
+    <link href="calender.css" type="text/css" rel="stylesheet" />
   </head>
   <body>
     <?php echo '<h1>welcome to my house</h1>'; ?> 
